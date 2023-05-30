193e0a33ae04e1868ef4e9eccabebf8580c6bd35
Proxymiity <dev@ayaya.red>
2023-05-30 19:55:24 +0200

    Added task creation endpoints

--- MangaDexZip/routes/mangadex.py
+++ MangaDexZip/routes/mangadex.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
 
 from typing import Union
 
@@ -8,17 +9,52 @@ from ..queue import client
 router = APIRouter(tags=["MangaDex"])
 
 
-@router.get("/title/{manga_id}", summary="Download a Manga")
-@router.get("/title/{manga_id}/{garbage}", summary="Download a Manga")
+class NewTask(BaseModel):
+    task_id: str
+
+
+@router.get("/title/{manga_id}", summary="Download a Manga (user-friendly)")
+@router.get("/manga/{manga_id}", include_in_schema=False)
+@router.get("/title/{manga_id}/{garbage}", include_in_schema=False)
+@router.get("/manga/{manga_id}/{garbage}", include_in_schema=False)
 def add_manga(manga_id: str,
               request: Request,
               garbage: Union[str, None] = None,
               light: Union[str, None] = None,
               lang: Union[str, None] = "en") -> RedirectResponse:
-    """*front-end use only* Download a Manga.
+    """Download a Manga.
 
-    For API usage, please refer to the /api/title endpoint."""
+    *front-end use only* - For API usage, please refer to the /api/manga endpoint.
+
+    *This endpoint is named '/title' to match MangaDex's frontend paths. It is also aliased to '/manga'.*"""
     _ = garbage
+    task = _add_manga(manga_id, request, light=light, lang=lang)
+
+    api_host = f"{request.url.hostname}:{request.url.port}" if request.url.port else request.url.hostname
+    api_url = f"{request.url.scheme}://{api_host}"
+    return RedirectResponse(f"{api_url}/queue/front/{task['task_id']}/wait")
+
+
+@router.get("/manga/{manga_id}", summary="Download a Manga (dev-friendly)")
+def add_manga(manga_id: str,
+              request: Request,
+              light: Union[str, None] = None,
+              lang: Union[str, None] = "en") -> NewTask:
+    """Download a Manga.
+
+    - `manga_id` must be a valid MangaDex Manga (Title).
+    - `light` is optional and refers to the downsized version of chapter images.
+    - `land` is optional, defaults to 'en' and refers to the language used when searching for chapters.
+
+    *developer use only* - For regular usage, please refer to the /title endpoint."""
+    task = _add_manga(manga_id, request, light=light, lang=lang)
+    return NewTask(task_id=task["task_id"])
+
+
+def _add_manga(manga_id: str,
+               request: Request,
+               light: Union[str, None] = None,
+               lang: Union[str, None] = "en"):
     worker = client.select_worker_auto()
     if not worker:
         raise HTTPException(status_code=503, detail="No reachable workers, please try again later")
@@ -30,22 +66,43 @@ def add_manga(manga_id: str,
     task = client.append(worker, "manga", manga_id, opt, request.client.host)
     if not task:
         raise HTTPException(status_code=502, detail="Couldn't reach worker, please try again later")
+    return task
+
+
+@router.get("/chapter/{chapter_id}", summary="Download a Chapter (user-friendly)")
+@router.get("/chapter/{chapter_id}/{garbage}", include_in_schema=False)
+def add_chapter(chapter_id: str,
+                request: Request,
+                garbage: Union[str, None] = None,
+                light: Union[str, None] = None) -> RedirectResponse:
+    """Download a Chapter.
+
+    *front-end use only* - For API usage, please refer to the /api/chapter endpoint."""
+    _ = garbage
+    task = _add_chapter(chapter_id, request, light=light)
 
     api_host = f"{request.url.hostname}:{request.url.port}" if request.url.port else request.url.hostname
     api_url = f"{request.url.scheme}://{api_host}"
     return RedirectResponse(f"{api_url}/queue/front/{task['task_id']}/wait")
 
 
-@router.get("/chapter/{chapter_id}", summary="Download a Chapter")
-@router.get("/chapter/{chapter_id}/{garbage}", summary="Download a Chapter")
+@router.get("/api/chapter/{chapter_id}", summary="Download a Chapter (dev-friendly)")
 def add_chapter(chapter_id: str,
                 request: Request,
-                garbage: Union[str, None] = None,
-                light: Union[str, None] = None) -> RedirectResponse:
-    """*front-end use only* Download a Chapter.
+                light: Union[str, None] = None) -> NewTask:
+    """Start a new Download Chapter Task.
 
-    For API usage, please refer to the /api/manga endpoint."""
-    _ = garbage
+    - `chapter_id` must be a valid MangaDex Chapter.
+    - `light` is optional and refers to the downsized version of chapter images.
+
+    *developer use only* - For regular usage, please refer to the /chapter endpoint."""
+    task = _add_chapter(chapter_id, request, light=light)
+    return NewTask(task_id=task["task_id"])
+
+
+def _add_chapter(chapter_id: str,
+                 request: Request,
+                 light: Union[str, None] = None):
     worker = client.select_worker_auto()
     if not worker:
         raise HTTPException(status_code=503, detail="No reachable workers, please try again later")
@@ -56,7 +113,4 @@ def add_chapter(chapter_id: str,
     task = client.append(worker, "chapter", chapter_id, opt, request.client.host)
     if not task:
         raise HTTPException(status_code=502, detail="Couldn't reach worker, please try again later")
-
-    api_host = f"{request.url.hostname}:{request.url.port}" if request.url.port else request.url.hostname
-    api_url = f"{request.url.scheme}://{api_host}"
-    return RedirectResponse(f"{api_url}/queue/front/{task['task_id']}/wait")
+    return task
